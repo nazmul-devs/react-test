a2598ea26120502e3ed6b35ab0a3f06bec40efb1
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-07-25 02:49:11 +0600

    create navbar and styled

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,45 @@
 import React from "react";
+import logo from "../assests/logo/logo.png";
 
 export const NavBar = () => {
 	return (
-		<div className="flex">
-			<div className="logo">LOGO</div>
-			<div className="navLink">Link here</div>
-			<div className="searchBox">Serch box</div>
+		<div className="flex justify-between bg-slate-200		px-16 items-center	">
+			<div className="logo">
+				<img className="p-2 w-20 cursor-pointer" src={logo} alt="LOGO" />
+			</div>
+			<div className="navLink">
+				<ul className="flex bold text-lg	">
+					<li className="px-6 border-r-4 border-slate-900	 cursor-pointer">
+						Topic
+					</li>
+					<li className="px-6 border-r-4 border-slate-900	 cursor-pointer">
+						Missions
+					</li>
+					<li className="px-6 border-r-4 border-slate-900	 cursor-pointer">
+						Galleries
+					</li>
+					<li className="px-6 border-r-4 border-slate-900	 cursor-pointer">
+						FLIGHTS TV
+					</li>
+					<li className="px-6 cursor-pointer">About</li>
+				</ul>
+			</div>
+			<div className="searchBox">
+				<form>
+					<input
+						className="p-2 mt-2"
+						type="text"
+						placeholder="Search.."
+						name="search"
+					/>
+					<button
+						className="p-2 mt-2 text-center bg-gray-100"
+						type="submit"
+					>
+						<i className="fa fa-search"></i>
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,9 @@ import { NavBar } from "../components/NavBar";
 
 export const Home = () => {
 	return (
-		<>
+		<div>
 			<NavBar />
 			<h4 className="text-center bold">Hello this is home</h4>
-		</>
+		</div>
 	);
 };
